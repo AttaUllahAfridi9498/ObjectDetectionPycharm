22a4a300f8788ce8dc19d822819c6827c942f7ba
AttaUllahAfridi9498 <mm.atta9498@gmail.com>
2024-04-05 15:38:48 +0500

    GUI Progress-bar

--- classwork/practice.py
+++ classwork/practice.py
@@ -20,12 +20,12 @@ def display():
 progressBar = ttk.Progressbar(frame, mode='determinate')
 progressBar.pack(padx=10, pady=10)
 
-button = ttk.Button(frame, text="Increment-20", command=increment)
+button = ttk.Button(frame, text="Increase", command=increment)
 button.pack(padx=10, pady=10, side=tk.LEFT)
 
-button = ttk.Button(frame, text="Decrement-20", command=decrement)
+button = ttk.Button(frame, text="Decrease", command=decrement)
 button.pack(padx=10, pady=10, side=tk.LEFT)
-button = ttk.Button(frame, text="Show-Result", command=display)
+button = ttk.Button(frame, text="Show", command=display)
 button.pack(padx=10, pady=10, side=tk.LEFT)
 
 frame.pack(padx=5, pady=5)
