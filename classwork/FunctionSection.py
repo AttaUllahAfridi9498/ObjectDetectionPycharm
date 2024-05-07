b6d2bf82779886630b03c04ac082b5a92794682a
AttaUllahAfridi9498 <mm.atta9498@gmail.com>
2024-05-07 23:11:37 +0500

    Parameterized Function

---
+++ classwork/FunctionSection.py
@@ -0,0 +1,31 @@
+#"PARAMETERIZED FUNCTIONS"
+
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+
+def message(a,b,c):
+    print("Enter a value: ")
+    print("The value of a is:",a,"\n The value of b is:",b,"\n The value of c is:",c)
+
+a1= int(input())
+b1= int(input())
+c1= int(input())
+
+def add(a1,b1,c1):
+    result= a1+b1+c1
+    print(result)
+
+
+message(a,b,c)
+add(3,2,4)
+
+"""
+"""
+number= input()
+def message(number):
+    print("Enter a number:", number)
+
+message(number)
+"""
