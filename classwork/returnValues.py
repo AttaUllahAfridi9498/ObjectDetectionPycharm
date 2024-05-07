d75a243798d18dd453ed28fcad1c24d825242ad8
AttaUllahAfridi9498 <mm.atta9498@gmail.com>
2024-05-07 23:17:44 +0500

    Return value simple task

---
+++ classwork/returnValues.py
@@ -0,0 +1,24 @@
+
+def happy_new_year():
+    a=5
+    b=3
+    result=a+b
+    convt=str(result)
+    name="Ali"
+    return 
+
+
+
+x=happy_new_year()
+print(x)
+y=happy_new_year()
+print(y)
+
+# def boring_function():
+#     print("'Boredom Mode' ON.")
+#     return "ali"
+#
+# print("This lesson is interesting!")
+# x=boring_function()
+# print(x)
+# print("This lesson is boring...")
