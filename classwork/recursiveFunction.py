602a28c0fb4be340c8f76c60c457e0edb8cb8d72
AttaUllahAfridi9498 <mm.atta9498@gmail.com>
2024-05-07 23:16:59 +0500

    Recursive Function

---
+++ classwork/recursiveFunction.py
@@ -0,0 +1,19 @@
+# Recursive implementation of the factorial function.
+
+def factorial(n):
+    if n == 1:    # The base case (termination condition.)
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(4)) # 4 * 3 * 2 * 1 = 24
+
+# def fun(a):
+#     if a > 30:
+#         return 3
+#     else:
+#         return a + fun(a + 3)
+#
+#
+# print(fun(25))
