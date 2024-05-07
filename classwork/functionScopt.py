e1698acbf072f9e12f8e767fb46973d49d688d8e
AttaUllahAfridi9498 <mm.atta9498@gmail.com>
2024-05-07 23:10:03 +0500

    Function Scope

---
+++ classwork/functionScopt.py
@@ -0,0 +1,41 @@
+#
+# def functionAddition():
+#     print("Hello, the value is:",value)
+#
+# value=5
+# # functionAddition()
+# def message():
+#     global alt
+#     alt = 1
+#     print("Hello, World!")
+#
+# y=alt
+# print(y)
+
+# def my_function():
+#     global var
+#     var = 2
+#     print("Do I know that variable?", var)
+#
+#
+# var = 1
+# my_function()
+def my_function():
+    global var
+    var = 2
+    print("Do I know that variable?", var)
+
+
+var = 1
+my_function()
+print(var)
+
+# def add():
+#     a=3
+#     b=4
+#     global c
+#     c=a+b
+# def showresult():
+#     print("The result is",c)
+#
+# showresult()
